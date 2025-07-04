58429cf62281a2450983e9ff003c03d4739ddfdd
Cheluri-AshokReddy <ashokreddy20020427@gmail.com>
2025-07-04 16:12:09 +0530

    Configure CORS to allow frontend domain

--- server.js
+++ server.js
@@ -8,7 +8,23 @@ const pool = require('./config/db');
 
 dotenv.config();
 
-app.use(cors());
+const allowedOrigins = [
+  'https://cheluri-ashokreddy.github.io',
+  'http://localhost:3000'
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', urlRoutes);
